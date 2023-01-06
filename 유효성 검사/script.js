c400f66a5d5283462e99b2e88bb51538539c9c11
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2023-01-06 20:01:15 +0900

    회원가입 기능 구현 완료

--- 유효성 검사/script.js
+++ 유효성 검사/script.js
@@ -1,29 +1,85 @@
+//입력 컨테이너
+let elContainer = document.querySelector(".container");
+//ID 영역
 let elInputUsername = document.querySelector("#username");
 let elFailureMessage = document.querySelector(".failure-message");
 
-//JS로 스타일 적용
-//elFailureMessage.style.display = "none";
+//PW 영역
+let elInputPW = document.querySelector("#password");
+let elFailurePW = document.querySelector(".failure-pw");
+let elInputRePW = document.querySelector("#retype-password");
+let elFailureRePW = document.querySelector(".failure-retype");
 
-//class를 이용해서 display 'none' 지정
-//elFailureMessage.classList.remove("hide");
+//회원가입 버튼
+let btnSignUp = document.querySelector(".signup");
+let valueId = false;
+let valuePW = false;
 
-let elSuccessMessage = document.querySelector(".success-message");
-
-//event와 enventHandler
-//키보드 입력시 함수 실행
+//ID 영역
 elInputUsername.onkeyup = function () {
-  console.log(elInputUsername.value);
-
-  //4글자보다 클때
-  if (isMoreThan4Length(elInputUsername.value)) {
-    elSuccessMessage.classList.remove("hide");
+  //4글자보다 크고 유효성 검사를 만족할 때
+  if (
+    isMoreThan4Length(elInputUsername.value) &&
+    onlyNumberAndEnglish(elInputUsername.value)
+  ) {
     elFailureMessage.classList.add("hide");
+    valueId = true;
   } else {
-    elSuccessMessage.classList.add("hide");
     elFailureMessage.classList.remove("hide");
+    if (elInputUsername.value === "") {
+      elFailureMessage.classList.add("hide");
+    }
+    valueId = false;
   }
 };
 
+//PW 유효성 검사
+elInputPW.onkeyup = function () {
+  //PW 조건에 맞을 때
+  if (strongPassword(elInputPW.value)) {
+    elFailurePW.classList.add("hide");
+  } else {
+    elFailurePW.classList.remove("hide");
+    if (elInputPW.value === "") {
+      elFailurePW.classList.add("hide");
+    }
+  }
+};
+
+//PW 일치 확인
+elInputRePW.onkeyup = function () {
+  if (isMatch(elInputPW.value, elInputRePW.value)) {
+    elFailureRePW.classList.add("hide");
+    valuePW = true;
+  } else {
+    elFailureRePW.classList.remove("hide");
+    if (elInputRePW.value === "") {
+      elFailureRePW.classList.add("hide");
+    }
+    valuePW = false;
+  }
+  console.log(isMatch(elInputPW.value, elInputRePW.value));
+};
+
+//회원가입 버튼 활성화 여부
+elContainer.onkeyup = function () {
+  if (valueId && valuePW) {
+    btnSignUp.disabled = false;
+  } else {
+    btnSignUp.disabled = true;
+  }
+};
+
+//ID 4글자
 function isMoreThan4Length(value) {
   return value.length >= 4;
+}
+
+//PW retype 비교
+function isMatch(pw1, pw2) {
+  if (pw1 === pw2) {
+    return true;
+  } else {
+    return false;
+  }
 }
